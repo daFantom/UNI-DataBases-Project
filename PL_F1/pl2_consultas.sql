044eaae94fe873dbf7ba4daef717b10b14021501
daFantom <emanuel.baciu@edu.uah.es>
2025-11-15 20:05:10 +0100

    Arreglado Consulta 7 para que recoga aquellos nombres repetidos pero que tienen distintos apellidos

--- PL_F1/pl2_consultas.sql
+++ PL_F1/pl2_consultas.sql
@@ -6,13 +6,13 @@
 --CONSULTA 1
 \echo "Consulta 1: Cantidad de Grandes Premios en los que se ha corrido en cada circuito en la base de datos, ordenados de mayor a menor."
 SELECT
-    c.name,
+    c.name AS nombreGP,
     COUNT(gp.name) as numero_de_grandes_premios
 FROM
     pl1_final.circuits_final c JOIN pl1_final.races_final gp
     ON c.circuitRef = gp.circuitRef
 GROUP BY 
-    c.name
+    nombreGP
 ORDER BY  
     numero_de_grandes_premios DESC;
 
@@ -20,14 +20,14 @@ ORDER BY
 
 -- CONSULTA 7
 SELECT
-    DISTINCT(pi.forename) AS nombre_piloto -- DISTINCT por si hay repetidos, aunque es la misma cantidad.
+    pi.forename AS nombre_piloto 
     FROM
         pl1_final.results_final AS r JOIN pl1_final.drivers_final AS pi ON
         r.pilotoRef = pi.driverRef
     WHERE
         r.posicion = 1
     GROUP BY
-        nombre_piloto
+        pi.driverRef
     ORDER BY
         nombre_piloto ASC; -- OPCIONAL, para que quede bonito.
 
