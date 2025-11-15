ad92b669db0b690f0a1bdfbe3184e9e37487a95f
daFantom <emanuel.baciu@edu.uah.es>
2025-11-15 19:22:33 +0100

    Consulta 7 hecha

--- PL_F1/pl2_consultas.sql
+++ PL_F1/pl2_consultas.sql
@@ -6,28 +6,28 @@
 --CONSULTA 1
 \echo "Consulta 1: Cantidad de Grandes Premios en los que se ha corrido en cada circuito en la base de datos, ordenados de mayor a menor."
 SELECT
-    c.circuitRef,
+    c.name,
     COUNT(gp.name) as numero_de_grandes_premios
 FROM
     pl1_final.circuits_final c JOIN pl1_final.races_final gp
     ON c.circuitRef = gp.circuitRef
 GROUP BY 
-    c.circuitRef
+    c.name
 ORDER BY  
     numero_de_grandes_premios DESC;
 
 
-/*
- CONSULTA 7
+
+-- CONSULTA 7
 SELECT
-    forename
+    DISTINCT(pi.forename) AS nombre_piloto -- DISTINCT por si hay repetidos, aunque es la misma cantidad.
     FROM
         pl1_final.results_final AS r JOIN pl1_final.drivers_final AS pi ON
-        r.driverRef = pi.driverRef
+        r.pilotoRef = pi.driverRef
     WHERE
-        posicion = 1;
-*/
-
+        posicion = 1
+    GROUP BY
+        nombre_piloto;
 
 
 ROLLBACK;
